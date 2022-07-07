7541c2e28e7c6a1299f7b46d1f123e17babaafb8
anthonytruongg <anthonytruongg1@gmail.com>
2022-07-07 01:10:34 -0700

    Finished skeleton

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";!function(){const e=document.querySelector(".content");document.querySelector(".header").appendChild(function(){const e=document.createElement("div");return e.setAttribute("class","home"),e.textContent="Home",e}()),e.appendChild(function(){const e=document.createElement("div");return e.setAttribute("class","header"),e.textContent="Home Page",e}())}()})();
